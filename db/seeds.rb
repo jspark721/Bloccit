10c2a5c21410551a5c8f0738f3e0041b41ae8027
Julie Park <jspark721@gmail.com>
2016-02-03 23:43:25 -0600

    Assignment 30 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,8 @@ require "random_data"
    )
  end
 
+ Post.find_or_create_by(title: "unique title", body: "unique body")
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
