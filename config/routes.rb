00ac7b307473ca271a33c1ddafd5219396cd3a22
Julie Park <jspark721@gmail.com>
2016-02-10 16:12:44 -0600

    Assignment 34 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   resources :topics do
+    resources :sponsored_posts
+  end
+
+  resources :topics do
     resources :posts, except: [:index]
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,9 +24,19 @@ topics = Topic.all
    )
   end
 
+10.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: Random.rand(100)
+  )
+end
+
 Post.find_or_create_by(title: "unique title", body: "unique body")
 
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{SponsoredPost.count} sponsored posts created"
