6835ba186b4cf43dfcff0d92ec0a7da5294df0cc
Julie Park <jspark721@gmail.com>
2016-02-15 23:47:11 -0600

    Checkpoint 40 authorization: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,9 +38,18 @@ topics = Topic.all
 
 Post.find_or_create_by(title: "unique title", body: "unique body")
 
-user = User.first
-user.update_attributes!(
-  email: 'juliepark21@gmail.com',
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
 
