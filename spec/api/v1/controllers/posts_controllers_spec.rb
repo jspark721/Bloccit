e152f3c4a806b389f07fb9c03bf9a962fa33f543
Julie Park <jspark721@gmail.com>
2016-02-26 16:10:07 -0600

    Assignment 48 completed

--- spec/api/v1/controllers/posts_controllers_spec.rb
+++ spec/api/v1/controllers/posts_controllers_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Api::V1::PostsController, type: :controller do
   let(:my_user) { create(:user) }
   let(:my_topic) { create(:topic) }
   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+  let(:my_comment) { Comment.create!(body: RandomData.random_paragraph, post: my_post, user: my_user)}
 
   context "unauthenticated user" do
     it "GET index returns http success" do
@@ -17,7 +18,7 @@ RSpec.describe Api::V1::PostsController, type: :controller do
     end
   end
 
-  context "unauthorized user" do
+  context "authenticated and unauthorized user" do
     before do
       controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
     end
@@ -30,6 +31,36 @@ RSpec.describe Api::V1::PostsController, type: :controller do
     it "GET show returns http success" do
       get :show, id: my_post.id
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  context "authenticated and authorized users" do
+    describe "GET index" do
+      before { get :index, id: my_post.id}
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns json content type" do
+        expect(response.content_type).to eq("application/json")
+      end
+    end
+
+    describe "GET show" do
+      before { get :show, id: my_post.id }
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns json content type" do
+        expect(response.content_type).to eq 'application/json'
+      end
+
+      it "returns child comments" do
+        expect(response.body).to eq(my_post.to_json(include: :comments))
+      end
     end
   end
 end
--- spec/api/v1/controllers/topics_controller_spec.rb
+++ spec/api/v1/controllers/topics_controller_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
  RSpec.describe Api::V1::TopicsController, type: :controller do
    let(:my_user) { create(:user) }
    let(:my_topic) { create(:topic) }
- 
+   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+
    context "unauthenticated user" do
      it "GET index returns http success" do
        get :index
@@ -29,6 +30,40 @@ require 'rails_helper'
      it "GET show returns http success" do
        get :show, id: my_topic.id
        expect(response).to have_http_status(:success)
+     end
+   end
+
+   context "authenticated and authorized users" do
+     describe "GET index" do
+       before { get :index, id: my_topic.id}
+
+       it "returns http success" do
+         expect(response).to have_http_status(:success)
+       end
+
+       it "returns json content type" do
+         expect(response.content_type).to eq("application/json")
+       end
+
+       it "returns child posts" do
+         expect(response.body).to eq([my_topic].to_json(include: :posts))
+       end
+     end
+
+     describe "GET show" do
+       before { get :show, id: my_topic.id }
+
+       it "returns http success" do
+         expect(response).to have_http_status(:success)
+       end
+
+       it "returns json content type" do
+         expect(response.content_type).to eq 'application/json'
+       end
+
+       it "returns child posts" do
+         expect(response.body).to eq(my_topic.to_json(include: :posts))
+       end
      end
    end
  end
